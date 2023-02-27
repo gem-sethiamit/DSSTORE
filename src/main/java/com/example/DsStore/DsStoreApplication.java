ab19834daa38c9f169ce57226cac40c23d4f51c3
Amit Sethi <Amit.Sethi@Geminisolutions.com>
2023-02-27 17:04:47 +0530

    dsstore intialized

---
+++ src/main/java/com/example/DsStore/DsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.DsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsStoreApplication.class, args);
+	}
+
+}
